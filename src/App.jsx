1caa1ae1be6dbff7ce9aa4b8e3a0f14536693691
Cortrax <aidenrphotos@gmail.com>
2023-10-14 18:57:02 -0400

    Connect fe and be with socketio

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
+import { io } from 'socket.io-client';
 
 function App() {
+    useEffect(() => {
+        const socket = io.connect('http://localhost:3001');
+        socket.on('connect', () => {
+            console.log('Socket connected', socket.id);
+        });
+
+        socket.on('disconnect', (reason) => {
+            console.log('Socket disconnected', reason);
+        });
+    });
+
     axios.get('http://localhost:3001').then((response) => {
         console.log(response);
     });
--- src/server.js
+++ src/server.js
@@ -1,16 +1,39 @@
 import express from "express";
-import { Socket } from "socket.io-client";
 import cors from 'cors';
+import {Server} from 'socket.io'
+import { createServer } from "node:http";
 
 const app = express();
+
+const corsConfig = {
+    origin: true,
+    credentials: true
+}
+
+app.use(cors(corsConfig));
+
+const httpServer = createServer();
+
+const io = new Server(httpServer, {
+    cors: {
+        origin: "http://localhost:3000"
+    }
+})
 const port = 3001;
 
-app.use(cors());
+io.on('connection', (socket) => {
+    console.log("user connected", socket.id)
+
+    socket.on('disconnect', (reason) => {
+        console.log("User disconnected", reason);
+    })
+})
+
 
 app.get('/', (req, res) => {
     res.send("Server sending");
 })
 
-app.listen(port, () => {
-    console.log(`Listening on port: ${port}`);
+httpServer.listen(3001, () => {
+    console.log("server running on port 3001");
 })
